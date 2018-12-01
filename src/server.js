8a73f50fd66c2000910f20cbd91ec4dcfa6b5b1b
Ha1f_Deaf <jaredtspears@gmail.com>
2018-12-01 14:34:37 -0500

    JOE: Added default get route that servers index.html

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+const app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
+app.get('/', (req, res) => {
+    console.log('A new user has made a GET request to "/"');
+    res.sendFile('dist/index.html', { root: __dirname});
+});
 console.log('server started '+ port);
