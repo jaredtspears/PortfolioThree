58b86fef6a96b9a6d6b3c4f3fe95b7224ea9ac7b
Ha1f_Deaf <jaredtspears@gmail.com>
2018-12-01 14:47:55 -0500

    Moved server.js up to root directory and adjusted route path of index.html

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 const app = express();
-// app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 app.get('/', (req, res) => {
     console.log('A new user has made a GET request to "/"');
-    res.sendFile('../dist/index.html', { root: __dirname});
+    res.sendFile('./dist/index.html', { root: __dirname});
 });
 console.log('server started '+ port);
