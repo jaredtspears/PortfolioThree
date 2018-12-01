44ffb8b5ad7dc9533f59ae489ff332873c6cef55
Ha1f_Deaf <jaredtspears@gmail.com>
2018-12-01 14:23:56 -0500

    deploying

---
+++ dist/animation.js
@@ -0,0 +1,10 @@
+//this is for the animation for the logo of the page, should slide in from the left to the right
+
+// have not written the function for this yet
+
+// eslint-disable-next-line no-undef
+var bounce = new Bounce();
+bounce.skew({
+  from: { x: 0, y: 0 },
+  to: { x: 20, y: 0 }
+});
